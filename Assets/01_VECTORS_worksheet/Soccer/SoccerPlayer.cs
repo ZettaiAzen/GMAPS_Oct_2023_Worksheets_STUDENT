df501091f0402f3cbb1a473e3b29f4d258362297
ZettaiAzen <jolin.wong112@gmail.com>
2023-10-24 13:55:58 +0800

    Update SoccerPlayer.cs

--- Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
+++ Assets/01_VECTORS_worksheet/Soccer/SoccerPlayer.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 public class SoccerPlayer : MonoBehaviour
 {
@@ -50,6 +52,31 @@ public class SoccerPlayer : MonoBehaviour
         }
         Debug.Log("Lowest Height: " + lowestHeight);
     }
+
+    SoccerPlayer FindClosestPlayerDot()
+    {
+        SoccerPlayer closest = null;
+        float minAngle = 180f;
+
+        foreach (SoccerPlayer other in OtherPlayers)
+        {
+            Vector3 toPlayer = (other.transform.position - this.transform.position);
+            toPlayer.Normalize();
+
+            float dot = Vector3.Dot(toPlayer, this.transform.position);
+            float angle = Mathf.Acos(dot);
+            angle *= Mathf.Rad2Deg;
+
+            if (angle < minAngle)
+            {
+                minAngle = angle;
+                closest = other;
+            }
+        }
+        return closest;
+        
+    }
+
     //float Magnitude(Vector3 vector)
     //{
 
@@ -72,13 +99,13 @@ public class SoccerPlayer : MonoBehaviour
     //    return closest;
     //}
 
-    void DrawVectors()
-    {
-        foreach (SoccerPlayer other in OtherPlayers)
-        {
-                Debug.DrawRay(transform.position, other.transform.position-transform.position, Color.black);
-        }
-    }
+    //void DrawVectors()
+    //{
+    //    foreach (SoccerPlayer other in OtherPlayers)
+    //    {
+    //            Debug.DrawRay(transform.position, other.transform.position-transform.position, Color.black);
+    //    }
+    //}
 
     void Update()
     {
@@ -91,7 +118,10 @@ public class SoccerPlayer : MonoBehaviour
             Debug.DrawRay(transform.position, transform.forward * 10f, Color.red);
         }
 
-        DrawVectors();
+        //DrawVectors();
+
+        SoccerPlayer targetPlayer = FindClosestPlayerDot();
+        targetPlayer.GetComponent<Renderer>().material.color = Color.yellow;
 
     }
 }
